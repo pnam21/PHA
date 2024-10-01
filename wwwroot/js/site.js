4d0944edeb6dc4cef406746561c07a19dd5384e0
quyetnb44 <quyetnbqe170091fpt.edu.vn>
2024-10-01 10:27:48 +0700

    updateUser

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleUserInfo() {
+    const userInfo = document.getElementById('userInfo');
+    userInfo.style.display = userInfo.style.display === 'block' ? 'none' : 'block'; // Hiện hoặc ẩn bảng thông tin
+}
