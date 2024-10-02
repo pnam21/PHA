af11f6650ffa6db4fd374dbf054caa22ff32397a
ADMIN <ADMIN@MOENGU>
2024-10-02 17:05:37 +0700

    update LoginController

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,24 +1,51 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using PHA.Models;
+using PHA.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 namespace PHA.Controllers
 {
-	public class LoginController : Controller
-	{
-		[Route("login")]
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return View("Login");
-		}
+    public class LoginController : Controller
+
+    {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(User model)
+        {
+            if (Users.IsNullOrEmpty())
+            {
+                ModelState.AddModelError("", "Email cannot be blank");
+                return View();
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                ModelState.AddModelError("", "Password cannot be blank");
+                return View();
+            }
+        }
+    }
 
 
-		// Deal with login logic
-		[Route("login")]
-		[HttpPost]
-		public IActionResult Login()
-		{ 
-			return Redirect("home");
-		}
-	}
-}
+
+            
